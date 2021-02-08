24320da77861d5a4c8a042ee4d41accde1f1f4c8
HumanBojack <spychalar59@gmail.com>
2021-02-08 13:29:51 +0100

    Added team and contact

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get '/contact', to: "contact#show"
+  get '/team', to: "team#show"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
