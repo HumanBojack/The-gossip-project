c19e1b7f0e1055af0fb81079b0216ca544467d8e
Day-101 <69306872+Day-101@users.noreply.github.com>
2021-02-10 00:19:48 +0100

    correction

--- app/models/gossip.rb
+++ app/models/gossip.rb
@@ -1,11 +1,7 @@
 class Gossip < ApplicationRecord
-<<<<<<< HEAD
 	validates :title, presence:true, length: { in: 3..14 }
 	validates :content, presence:true
-=======
-	validates :title, presence true, length: { minimum: 3 }
-	validates :content, presence true,
->>>>>>> 54f104284a5046e5b0547da049472a6b137e31f8
+
 	belongs_to :user
 	has_many :given_tags
 end
