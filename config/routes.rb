14fe493c76e0d3020d553acdf4ace608aeacd0e9
Day-101 <69306872+Day-101@users.noreply.github.com>
2021-02-09 16:51:02 +0100

    REST Style failed

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
-	root "gossips#list"
-	get "gossips/:id", to: "gossips#show"
-  get 'welcome/:name', to: "welcome#show"
-  get '/contact', to: "contact#show"
-  get '/team', to: "team#show"
-  get 'author/:id', to: "author#show"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :gossips
+  resources :gossips do
+    resources :comments
+  end
+  # root "gossips#list"
+	# get "gossips/:id", to: "gossips#show"
+  # get 'welcome/:name', to: "welcome#show"
+  # get '/contact', to: "contact#show"
+  # get '/team', to: "team#show"
+  # get 'author/:id', to: "author#show"
 end
