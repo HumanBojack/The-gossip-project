da069374548bd662b04d19011f0f422871e7aff1
HumanBojack <spychalar59@gmail.com>
2021-02-10 16:44:41 +0100

    Comments are now editables and deletables

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,8 +6,26 @@ class CommentsController < ApplicationController
 		@comment.update(gossip: Gossip.find(params[:gossip_id]), user: User.find_by(first_name: "Anonymous"))
 
 	 @comment.save
-			redirect_to gossip_path(params[:gossip_id])
+	 redirect_to gossip_path(params[:gossip_id])
+	end
 
+	def edit
+		@gossip = Gossip.find(params[:gossip_id])
+		@comment = Comment.find(params[:id])
+	end
+
+	def update
+		@comment = Comment.find(params[:id])
+		@comment.update(comment_params)
+
+		@comment.save
+		redirect_to gossip_path(params[:gossip_id])
+	end
+
+	def destroy
+		@comment = Comment.find(params[:gossip_id])
+		@comment.destroy
+		redirect_to gossip_path(params[:id])
 	end
 
 	private
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ApplicationRecord
 	validates :user, presence: true
-	validates :content, length: {in: 10..100}
+	validates :content, length: {in: 2..100}
 	belongs_to :user
 	belongs_to :gossip
 end
